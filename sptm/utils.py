72290da56d0b7b6cf3833633c169cd0a42a17b71
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2018-07-14 09:42:30 +0530

    Added raise TypeError, commented in napoleon style

--- sptm/utils.py
+++ sptm/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-	Utility functions
+    Utility functions
 """
 
 #######################################
@@ -9,61 +9,62 @@
 __author__ = "Rochan Avlur Venkat"
 __credits__ = ["Anupam Mediratta"]
 __license__ = "MIT"
-__version__ = "0.1"
+__version__ = "1.0"
 __maintainer__ = "Rochan Avlur Venkat"
 __email__ = "rochan170543@mechyd.ac.in"
 
 #######################################
 
 def force_unicode(string, encoding='utf-8', errors='ignore'):
-	"""
-	Returns a unicode object representing 'string'. Treats bytestrings using the
-	'encoding' codec.
+    """Force converts a string to unicode object
 
-	Arguments
-	---------
-	string: string to be encoded
-	encoding: encoding type, defaults to `utf-8`
-	errors: whether or not to ignore errors, defaults to `ignore`
+    Treats bytestrings using the 'encoding' codec.
 
-	Returns
-	---------
-	unicode string
-	"""
+    Args:
+        string: string to be encoded
+        encoding: encoding type, defaults to `utf-8`
+        errors: whether or not to ignore errors, defaults to `ignore`
 
-	if string is None:
-		return ''
+    Returns:
+        unicode object
 
-	try:
-		if not isinstance(string, basestring,):
-			if hasattr(string, '__unicode__'):
-				string = unicode(string)
-			else:
-				try:
-					string = unicode(str(string), encoding, errors)
-				except UnicodeEncodeError:
-					if not isinstance(string, Exception):
-						raise
-					# If we get to here, the caller has passed in an Exception
-					# subclass populated with non-ASCII data without special
-					# handling to display as a string. We need to handle this
-					# without raising a further exception. We do an
-					# approximation to what the Exception's standard str()
-					# output should be.
-					string = ' '.join([force_unicode(arg, encoding, errors) for arg in string])
-		elif not isinstance(string, unicode):
-			# Note: We use .decode() here, instead of unicode(string, encoding,
-			# errors), so that if string is a SafeString, it ends up being a
-			# SafeUnicode at the end.
-			string = string.decode(encoding, errors)
-	except UnicodeDecodeError, e:
-		if not isinstance(string, Exception):
-			raise UnicodeDecodeError(string, *e.args)
-		else:
-			# If we get to here, the caller has passed in an Exception
-			# subclass populated with non-ASCII bytestring data without a
-			# working unicode method. Try to handle this without raising a
-			# further exception by individually forcing the exception args
-			# to unicode.
-			string = ' '.join([force_unicode(arg, encoding, errors) for arg in string])
-	return string
+    Raises:
+        TypeError: string argument left empty
+    """
+
+    if string is None:
+        raise TypeError('Function call has string argument left empty')
+
+    try:
+        if not isinstance(string, basestring,):
+            if hasattr(string, '__unicode__'):
+                string = unicode(string)
+            else:
+                try:
+                    string = unicode(str(string), encoding, errors)
+                except UnicodeEncodeError:
+                    if not isinstance(string, Exception):
+                        raise
+                    # If we get to here, the caller has passed in an Exception
+                    # subclass populated with non-ASCII data without special
+                    # handling to display as a string. We need to handle this
+                    # without raising a further exception. We do an
+                    # approximation to what the Exception's standard str()
+                    # output should be.
+                    string = ' '.join([force_unicode(arg, encoding, errors) for arg in string])
+        elif not isinstance(string, unicode):
+            # Note: We use .decode() here, instead of unicode(string, encoding,
+            # errors), so that if string is a SafeString, it ends up being a
+            # SafeUnicode at the end.
+            string = string.decode(encoding, errors)
+    except UnicodeDecodeError, e:
+        if not isinstance(string, Exception):
+            raise UnicodeDecodeError(string, *e.args)
+        else:
+            # If we get to here, the caller has passed in an Exception
+            # subclass populated with non-ASCII bytestring data without a
+            # working unicode method. Try to handle this without raising a
+            # further exception by individually forcing the exception args
+            # to unicode.
+            string = ' '.join([force_unicode(arg, encoding, errors) for arg in string])
+    return string
